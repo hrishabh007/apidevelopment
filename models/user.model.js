ce92f31bebf59871f6123edcb136ec6b39394677
Rishabh Balani <rishibalani007@gmail.com>
2025-11-20 08:31:18 +0530

    awt token in process

---
+++ models/user.model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const {Mongoose} = require("mongoose");
+const userSchema = new Mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now
+    }
+})
+
+const User = mongoose.model('User',userSchema);
+module.exports = User;
